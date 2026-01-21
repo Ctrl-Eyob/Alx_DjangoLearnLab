84a85c72c5c0b1fe3e8629397299560da1be1ba6
Ctrl-Eyob <eyobaberabekru@gmail.com>
2026-01-21 17:30:50 +0300

    fixed views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views.generic import DetailView
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
 from .models import Book, Library
 
-## function based view=
+
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-## class bassed view=
+    return render(
+        request,
+        "relationship_app/list_books.html",
+        {"books": books}
+    )
+
+
 class LibraryDetailView(DetailView):
-    model= Library
-    template_name = 'relationshi_app/library_detail.html'
-    context_object_name ='library'
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
