9d0e417681c65441d61da4ce046ee8b3c06cc63f
Ctrl-Eyob <eyobaberabekru@gmail.com>
2026-01-21 17:48:59 +0300

    added login, logout register templates and linked them to the correct urls

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,10 @@ from .models import Library
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book, Library
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
 
 def list_books(request):
@@ -17,3 +21,26 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
+class UserLoginView(LoginView):
+    template_name = "relationship_app/login.html"
+
+class UserLogoutView(LogoutView):
+    template_name = "relationship_app/logout.html"
+
+    
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("list_books")
+    else:
+        form = UserCreationForm()
+
+    return render(
+        request,
+        "relationship_app/register.html",
+        {"form": form}
+    )
