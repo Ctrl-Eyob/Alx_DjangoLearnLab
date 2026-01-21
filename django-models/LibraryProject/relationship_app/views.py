7fcc47bbdbcad5e1c6cae292fb586bfcccf7824c
Ctrl-Eyob <eyobaberabekru@gmail.com>
2026-01-21 18:26:49 +0300

    implemented custom permissions for book

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 
 def list_books(request):
     books = Book.objects.all()
@@ -68,3 +69,18 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, "relationship_app/member_view.html")
+
+
+@permission_required("relationship_app.can_add_book")
+def add_book(request):
+    return render(request, "relationship_app/add_book.html")
+
+
+@permission_required("relationship_app.can_change_book")
+def edit_book(request, book_id):
+    return render(request, "relationship_app/edit_book.html")
+
+
+@permission_required("relationship_app.can_delete_book")
+def delete_book(request, book_id):
+    return render(request, "relationship_app/delete_book.html")
