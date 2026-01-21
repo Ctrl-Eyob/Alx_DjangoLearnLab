73479a156c9c341a03c96434c363f55a67272c92
Ctrl-Eyob <eyobaberabekru@gmail.com>
2026-01-21 18:05:29 +0300

    created role templates,role restricted views, user with user profile view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 from .views import UserLoginView, UserLogoutView, register
-
+from . import views
 urlpatterns = [
     path("books/", views.list_books, name="list_books"),
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
@@ -17,6 +17,9 @@ urlpatterns = [
         name="logout",
     ),
     path("register/", views.register, name="register"),
+    path("admin/", views.admin_view, name="admin_view"),
+    path("librarian/", views.librarian_view, name="librarian_view"),
+    path("member/", views.member_view, name="member_view"),
 ]
 
-]
+
