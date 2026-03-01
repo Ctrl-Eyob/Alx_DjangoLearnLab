1524cb97f8a9ea9939daea2d5ddcb210632f0c8c
Ctrl-Eyob <eyobaberabekru@gmail.com>
2026-03-01 18:00:43 +0300

    installed post app

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,24 +1,12 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
-from .models import Post, Comment
 
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = "__all__"
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = "__all__"
 User = get_user_model()
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField()
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
@@ -30,11 +18,8 @@ class RegisterSerializer(serializers.ModelSerializer):
             "bio",
             "profile_picture",
         ]
-        extra_kwargs = {
-            "password": {"write_only": True}
-        }
 
     def create(self, validated_data):
-        user = get_user_model().objects.create_user(**validated_data)
+        user = User.objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,19 +1,7 @@
 from rest_framework import generics, permissions
 from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer
-from rest_framework import viewsets
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
 
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by("-created_at")
-    serializer_class = PostSerializer
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
 User = get_user_model()
 
 
